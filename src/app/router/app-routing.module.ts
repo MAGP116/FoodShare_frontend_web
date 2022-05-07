abfa9ef8241619e2993d07c6c733b00632574d0e
miguel partida <ma.gonzalezp33@gmail.com>
2022-05-07 09:34:49 -0500

    created login page

---
+++ src/app/router/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core'
+import {Routes, RouterModule} from '@angular/router'
+import { HomeComponent } from '../pages/home/home.component';
+import { LoginComponent } from '../pages/login/login.component';
+import { NotFoundComponent } from '../pages/not-found/not-found.component';
+import { AuthGuard } from '../guards/auth.guard';
+
+const routes: Routes = [
+    {path:'', component:LoginComponent},
+    {path: "home", component:HomeComponent, canActivate:[AuthGuard],},
+    {path: '**', component:NotFoundComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{}
