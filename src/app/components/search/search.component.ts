ab9656fbe14eb0f2d5e8a9f223e7abbaa3ffc10a
Armando Ferreira <is722820@iteso.mx>
2022-05-09 17:58:54 -0500

    Search user template

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchUserService } from '../../services/search-user.service';
-import { debounceTime } from 'rxjs/operators';
-import { fromEvent, Subject } from 'rxjs';
-import { FormControl } from '@angular/forms';
+import {
+  SearchUserService,
+  UserSearch,
+} from '../../services/search-user.service';
+import { debounceTime, forkJoin, map, of, startWith, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -10,23 +11,30 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-  data = [];
-  subject: Subject<any> = new Subject();
-  autoCompletFormControl = new FormControl();
+  data: UserSearch[] = [];
+  searchUser$ = new Subject();
   constructor(private readonly SearchUserService: SearchUserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    
+  }
   getUsersSearch(name: any) {
     const keywork = name.target.value;
-    // const debounceExample = keywork.pipe(debounce(() => timer(6000)));
-    // const result = debounceExample.subscribe((val: any) => {
-    //   console.log('Val:', val);
-    //   console.log("Val2: ", debounceExample);
-    // });
 
-    this.SearchUserService.getUser(keywork).subscribe((response: any) => {
-      this.data = response
-      console.log(this.data);
-    });
+    // const debounceExample = keywork.pipe(debounceTime(3000));
+    // console.log(debounceExample);
+    
+    this.searchUser$
+      .pipe(
+        startWith(''),
+        debounceTime(2000),
+        switchMap(() => {
+          return forkJoin([this.SearchUserService.getUser(keywork)]);
+        })
+      )
+      .subscribe((response:any)=>{
+        console.log(response)
+        this.data = response
+      });
   }
 }
