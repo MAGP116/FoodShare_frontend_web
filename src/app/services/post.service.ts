52de12cf514a330b96181dc2e1f82e610b865bc8
miguel partida <ma.gonzalezp33@gmail.com>
2022-05-08 21:08:38 -0500

    profile posts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {UserPreviewInterface} from '../services/user.service'
+import {UserPreviewInterface} from './user.service'
 
 
 export interface PostInterface {
@@ -14,13 +14,19 @@ export interface PostInterface {
 @Injectable({
   providedIn: 'root',
 })
-export class HomeService {
+export class PostService {
   private url = 'http://localhost:3000/'; //TODO: CHANGE TO REAL URL
 
   constructor(private readonly http: HttpClient) {}
 
   getPosts() {
     return this.http.get<PostInterface[]>(`${this.url}post`, {
+      withCredentials: true,
+    });
+  }
+
+  getUserPosts(id:string){
+    return this.http.get<PostInterface[]>(`${this.url}post/user/${id}`, {
       withCredentials: true,
     });
   }
